9e6f5c5795e3463d1d8de24db8cf454dff2deec9
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-11-10 20:00:08 +0100

    [Dynamically build up the outline based on (to be rendered) parts] - Use EModelService to find all parts - Check parent of parent (Sash) if to be rendered

--- e4/src/e4/Outline.java
+++ e4/src/e4/Outline.java
@@ -1,10 +1,13 @@
 package e4;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -14,24 +17,19 @@ import org.eclipse.swt.widgets.Label;
 public class Outline {
 	
 	@PostConstruct
-	public void postConstruct(Composite parent, Speaker speaker) {
+	public void postConstruct(Composite parent, Speaker speaker, EModelService ems, MApplication mApp) {
 		
 		Group grpOutline = new Group(parent, SWT.NONE);
 		grpOutline.setText("Outline for speaker: " + speaker.getName());
 		grpOutline.setLayout(new FillLayout(SWT.VERTICAL));
 		
-		Button btnFirstPart = new Button(grpOutline, SWT.CHECK);
-		btnFirstPart.setText("First part");
-		
-		new Label(grpOutline, SWT.SEPARATOR | SWT.HORIZONTAL);
-		
-		final Button btnSecondPart = new Button(grpOutline, SWT.CHECK);
-		btnSecondPart.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println(btnSecondPart.getText());
+		List<MPart> elements = ems.findElements(mApp, null, MPart.class, null);
+		for (MPart mPart : elements) {
+			if (mPart.getParent().getParent().isToBeRendered()) {
+				Button btnFirstPart = new Button(grpOutline, SWT.CHECK);
+				btnFirstPart.setText(mPart.getLabel() == null ? "" : mPart.getLabel());
+				new Label(grpOutline, SWT.SEPARATOR | SWT.HORIZONTAL);
 			}
-		});
-		btnSecondPart.setText("Second part");
+		}
 	}
 }
