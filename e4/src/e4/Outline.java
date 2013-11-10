68dfdc5dc9ed1678a1225de40d6ab54ec676bcfa
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-11-10 15:45:08 +0100

    [Add Outline slide to presentation] - Create MPart model object directly under TrimmedWindow - Generate implementation class 'Outline' and a @PostConstruct method

---
+++ e4/src/e4/Outline.java
@@ -0,0 +1,12 @@
+package e4;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Outline {
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+	}
+}
