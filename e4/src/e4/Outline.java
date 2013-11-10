b08ac64512f77b4b8bec9ad496d15212429d1f76
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-11-10 15:53:12 +0100

    [Have an outline that shows the current chapter of the presentation] - Create part impl with WindowBuilder - One button per chapter - Show SWT listener concept

--- e4/src/e4/Outline.java
+++ e4/src/e4/Outline.java
@@ -2,11 +2,36 @@ package e4;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 
 public class Outline {
 	
 	@PostConstruct
 	public void postConstruct(Composite parent) {
+		
+		Group grpOutline = new Group(parent, SWT.NONE);
+		grpOutline.setText("Outline");
+		grpOutline.setLayout(new FillLayout(SWT.VERTICAL));
+		
+		Button btnFirstPart = new Button(grpOutline, SWT.CHECK);
+		btnFirstPart.setText("First part");
+		
+		new Label(grpOutline, SWT.SEPARATOR | SWT.HORIZONTAL);
+		
+		final Button btnSecondPart = new Button(grpOutline, SWT.CHECK);
+		btnSecondPart.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println(btnSecondPart.getText());
+			}
+		});
+		btnSecondPart.setText("Second part");
 	}
 }
