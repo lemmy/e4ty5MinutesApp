d93107d03845ba397d3210d57e77c4c6e3d31bae
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-11-10 21:34:45 +0100

    [Show services registered by impl bundle and the service consumers] - Use OSGi api to find registered services and using bundles

--- impl/src/impl/Component.java
+++ impl/src/impl/Component.java
@@ -1,7 +1,9 @@
 package impl;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
 import api.ContentService;
 
@@ -9,9 +11,27 @@ public class Component implements ContentService {
 
 	@Override
 	public String getSlideContent() {
-		String version = FrameworkUtil.getBundle(getClass()).getHeaders()
+		final Bundle bundle = FrameworkUtil.getBundle(getClass());
+		
+		String version = bundle.getHeaders()
 				.get(Constants.BUNDLE_VERSION);
-		return "Content coming from bundle with version: " + version;
+		String msg = "Content coming from bundle with version: " + version;
+		
+		ServiceReference<?>[] registeredServices = bundle.getRegisteredServices();
+		if(registeredServices != null) {
+			for (int i = 0; i < registeredServices.length; i++) {
+				Bundle[] usingBundles = registeredServices[i].getUsingBundles();
+				if (usingBundles != null) {
+					msg += registeredServices[i].toString() + " used by: ";
+					for (int j = 0; j < usingBundles.length; j++) {
+						Bundle b = usingBundles[j];
+						msg += "\n" + b.getSymbolicName();
+					}
+				}
+			}
+		}
+		
+		return msg;
 	}
 
 }
