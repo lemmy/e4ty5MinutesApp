bb52788f18e305807da52a5aabb135b6b60d6289
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-11-10 20:49:32 +0100

    [Mark chapters of presentation done when slide is on top] - Use EventTopic (automatgically in UI thread) to receive notification about part activation - set button to checked

--- e4/src/e4/Outline.java
+++ e4/src/e4/Outline.java
@@ -1,11 +1,17 @@
 package e4;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
@@ -16,6 +22,8 @@ import org.eclipse.swt.widgets.Label;
 
 public class Outline {
 	
+	private final Map<MPart, Button> buttons = new HashMap<MPart, Button>();
+
 	@PostConstruct
 	public void postConstruct(Composite parent, Speaker speaker, EModelService ems, MApplication mApp) {
 		
@@ -28,8 +36,19 @@ public class Outline {
 			if (mPart.getParent().getParent().isToBeRendered()) {
 				Button btnFirstPart = new Button(grpOutline, SWT.CHECK);
 				btnFirstPart.setText(mPart.getLabel() == null ? "" : mPart.getLabel());
+				buttons .put(mPart, btnFirstPart);
 				new Label(grpOutline, SWT.SEPARATOR | SWT.HORIZONTAL);
 			}
+		}
+	}
+	
+	@Inject
+	@Optional
+	private void updateButtons(
+			@UIEventTopic(UIEvents.UILifeCycle.BRINGTOTOP) org.osgi.service.event.Event e) {
+		final Object key = e.getProperty(UIEvents.EventTags.ELEMENT);
+		if (buttons.containsKey(key)) {
+			buttons.get(key).setSelection(true);
 		}
 	}
 }
